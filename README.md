f2015abcc81d16dbcdbab884180b77358565702a
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2019-09-16 23:42:19 +0900

    Add misc/version-checker.py

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ This repository is aimed for following:
 
 ## environment
 
-- python 3.x
-- TensorFlow 1.x.x
-- Keras x.x.x
+ - Python ver : 3.6.3
+ - Numpy ver : 1.16.2
+ - Scipy ver : 1.2.1
+ - Pandas ver : 0.24.2
+ - scikit-learn ver : 0.20.3
+ - matplotlib ver : 3.0.3
+ - TensorFlow ver : 1.13.1
+ - TensorFlow-Keras ver : 2.2.4-tf
---
+++ misc/version-checker.py
@@ -0,0 +1,17 @@
+import platform
+import numpy as np
+import scipy as sp
+import pandas as pd
+import sklearn as sk
+import matplotlib as mp
+import tensorflow as tf
+from tensorflow import keras
+
+print('Python ver : ' + platform.python_version())
+print('Numpy ver : ' + np.__version__)
+print('Scipy ver : ' + sp.__version__)
+print('Pandas ver : ' + pd.__version__)
+print('scikit-learn ver : ' + sk.__version__)
+print('matplotlib ver : ' + mp.__version__)
+print('TensorFlow ver : ' + tf.__version__)
+print('TensorFlow-Keras ver : ' + keras.__version__)
