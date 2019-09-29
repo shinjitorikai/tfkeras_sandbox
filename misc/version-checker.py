f2c5604e48b4003b49dee9a697061c126ab9cacf
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2019-09-29 10:38:50 +0900

    misc/version-checker.py Add opencv-python module version output.

--- misc/version-checker.py
+++ misc/version-checker.py
@@ -6,6 +6,7 @@ import sklearn as sk
 import matplotlib as mp
 import tensorflow as tf
 from tensorflow import keras
+import cv2
 
 print('Python ver : ' + platform.python_version())
 print('Numpy ver : ' + np.__version__)
@@ -15,3 +16,4 @@ print('scikit-learn ver : ' + sk.__version__)
 print('matplotlib ver : ' + mp.__version__)
 print('TensorFlow ver : ' + tf.__version__)
 print('TensorFlow-Keras ver : ' + keras.__version__)
+print('opencv-python ver : ' + cv2.__version__)
