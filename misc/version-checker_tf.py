c9d0286050404c4d1b994d42ab4e6b71c765cb02
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-10-12 00:01:02 +0900

    Modify misc/version-checker_tf.py (move tfhub to another file)

--- misc/version-checker_tf.py
+++ misc/version-checker_tf.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
-import tensorflow_hub as hub
+from tensorflow.python.client import device_lib
 
 print('TensorFlow ver', tf.__version__)
-print('TensorFlow Hub ver', hub.__version__)
 print('Eager mode : ', tf.executing_eagerly())
 if tf.config.experimental.list_physical_devices('GPU'):
     print('GPU : available')
 else:
     print('GPU : not available')
+
+print('local devices:')
+print(device_lib.list_local_devices())
---
+++ misc/version-checker_tfhub.py
@@ -0,0 +1,3 @@
+import tensorflow_hub as hub
+
+print('TensorFlow Hub ver', hub.__version__)
